05aca595dc1eadd9f5d12a3641fedb08a96d9ec7
Moon-12 <ashwijanayak@gmail.com>
2025-05-11 14:39:31 -0400

    property bookmark working

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -181,7 +181,7 @@ const NavBar = () => {
               {isProfileMenuOpen && (
                 <div
                   id="user-menu"
-                  className=" absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  className=" absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-opacity-5 focus:outline-none"
                   role="menu"
                   aria-orientation="vertical"
                   aria-labelledby="user-menu-button"
@@ -200,7 +200,7 @@ const NavBar = () => {
                     Your Profile
                   </Link>
                   <Link
-                    href="/saved-properties"
+                    href="/properties/saved"
                     className="block px-4 py-2 text-sm text-gray-700"
                     role="menuitem"
                     tabIndex="-1"
